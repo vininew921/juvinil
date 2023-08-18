2f90fa7b6219d603f6397ba6717af90ae9bbcd49
Vinicius Barbosa <vininew921@hotmail.com>
2023-08-18 12:23:13 -0300

    Improved performance and architecture

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, thiserror::Error)]
 pub enum JuvinilError {
     #[error("Couldn't match token {0} to any defined expression - line {1}")]
-    NoOptionError(String, usize),
+    NoRegexMatch(String, usize),
 }
 
 pub type JuvinilResult<T, E = JuvinilError> = anyhow::Result<T, E>;
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,23 @@ use std::fs;
 
 pub mod error;
 pub mod lex;
+pub mod token_map;
 
 fn main() {
+    tracing_subscriber::fmt().pretty().init();
+
     let file_path = "src/test.jv";
     let file_result = fs::read_to_string(file_path);
 
     if let Ok(file) = file_result {
-        if let Err(x) = lex::process_file_content(file) {
-            println!("Error processing file contents: {}", x);
+        if let Err(err) = lex::process_file_content(file) {
+            tracing::error!("Error processing file contents: {}", err);
             return;
         }
 
-        println!("Success!");
+        tracing::info!("Success!");
         return;
     }
 
-    println!("Error opening file {file_path}");
+    tracing::error!("Error opening file {file_path}");
 }
